c059237118e4f7e89ac7e82cb4635943d6d0fefd
Timilehin Adegoju <rehobothtimilehin17@gmail.com>
2022-09-08 15:42:13 -0700

    print output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
- */
+ *
+ *  * main - main block
+ *
+ *   * return: 0
+ *
+ *    * Return: 0
+ *
+ *     */
+
  int main(void)
+
+	 {
+
+		  puts("\"Programming is like building a multilingual puzzle");
+
+		   returns (0);
+
+		    }
+
+int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
