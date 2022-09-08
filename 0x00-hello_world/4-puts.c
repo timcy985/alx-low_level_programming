86737c8b21537e1c9b37b2519cf96bb9f05a11da
Timilehin Adegoju <rehobothtimilehin17@gmail.com>
2022-09-08 11:07:22 -0700

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
 * main - print the string in the put function
 * 
-*Description: using the main function
-* This program prints "programming is like building a multilingual puzzle"
+* Description: using the main function
+*this program prints "programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-     puts("\"programming is like building a multilingual puzzle");
-     return (0);
-}
+      puts("\"programming is like building a multilingual puzzle);
+      return (0);
