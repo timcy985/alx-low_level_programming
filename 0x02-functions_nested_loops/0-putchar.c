29c1d870f8de0a6d78b42245e1aa9c6cf8c43eb7
Timilehin Adegoju <rehobothtimilehin17@gmail.com>
2022-09-14 02:51:22 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
 /*
- * File: 0-putchar.c
- * Auth: Ebenezer Sam-Oladapo
+ * The file:  0-putchar.c
  */
- #include "main.h"
- /**
-  * main - This code prints _putchar
-  * Return: 0 on success
-  */
-  int main(void)
-  {
-  char c[] = "_putchar";
-  int i;
-  for (i = 0; i < 8; i++)
-  {
-  _putchar(c[i]);
-  }
-  _putchar('\n');
-  _putchar('\n');
-  }
+#include "main.h"
+/*
+ * main - This code prints _putchar
+ * Return: 0 on success
+ */
+ int main(void)
+ {
+ char c[] = "_putchar";
+ int i;
+ for (i = 0; i < 6; i++)
+ for (i = 0; i < 8; i++)
+ {
+ _putchar(c[i]);
+ }
