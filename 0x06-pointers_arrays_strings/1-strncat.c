2a850cd397767ffdd06022e9f48d1dc72f31b566
Timilehin Adegoju <rehobothtimilehin17@gmail.com>
2022-09-22 04:08:36 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dlen = 0, j = 0;
 
-
 	while (dest[dlen])
 	{
 		dlen++;
-
+	
 	}
 
 	while (j < n && src[j])
@@ -24,10 +23,11 @@ char *_strncat(char *dest, char *src, int n)
 		dest[dlen] = src[j];
 		dlen++;
 		j++;
+
 	}
 
 	dest[dlen + n + 1] = '\0';
 
-        return (dest);
+	return (dest);
 
 }
