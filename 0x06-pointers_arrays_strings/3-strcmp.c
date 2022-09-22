feda63c497c427c36f247df951787d5e4f458208
Timilehin Adegoju <rehobothtimilehin17@gmail.com>
2022-09-22 06:57:39 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,12 @@
-
 #include "main.h"
 
 /**
- *  _strcmp - Compares two strings
- *  @s1: The first string
- *  @s2: The second string
- *
- *  Return: int value
- */
+  *  _strcmp - Compares two strings
+  *   @s1: The first string
+  *   @s2: The second string
+  *
+  *   Return: int value
+  */
 int _strcmp(char *s1, char *s2)
 {
 	int a = 0, b = 0, c = 0, r = 0, lim;
@@ -15,48 +14,39 @@ int _strcmp(char *s1, char *s2)
 	while (s1[a])
 	{
 		a++;
-
-	}
-
-
-	while (s2[b])
-	{
-		b++;
-
 	}
 
 	while (s2[b])
 	{
-
 		b++;
-
 	}
 
 	if (a <= b)
 	{
 		lim = a;
-
 	}
 	else
 	{
 		lim = b;
-
 	}
 
 	while (c <= lim)
 	{
 		if (s1[c] == s2[c])
-	        {
+		{
 			c++;
 			continue;
 		}
 		else
 		{
 			r = s1[c] - s2[c];
+			break;
 		}
 
 		c++;
 	}
 
-	return (r);
-  }
+	return (r)
+} 
+
+
